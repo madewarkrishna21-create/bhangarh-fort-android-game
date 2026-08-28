54d16e3f7ca51947f70a1d307c7f4c06ab98640b
madewarkrishna21-create <madewarkrishna21@gmail.com>
2026-08-28 12:31:58 +0530

    Enable Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,21 +4,25 @@ plugins {
 }
 
 android {
-    namespace = "com.bhangarhfort.game"
+    namespace = "com.bhangerhfort"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.bhangarh.fort.game"
+        applicationId = "com.bhangerhfort"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
+    }
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    buildFeatures {
+        compose = true
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.ui:ui:1.7.8")
+    implementation("androidx.compose.material3:material3:1.3.1")
 }
