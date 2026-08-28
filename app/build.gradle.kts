cf839c819ebfe7554e05294ea3a0b8d0d0b5f4a7
madewarkrishna21-create <madewarkrishna21@gmail.com>
2026-08-28 12:19:39 +0530

    Create app/build.gradle.kts
    Create Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.bhangarhfort.game"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.bhangarh.fort.game"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
